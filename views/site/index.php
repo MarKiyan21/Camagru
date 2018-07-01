718954bec82fa1ffcc01253d095a0842c39e6236
marian <marian@MacBook-Pro-Mac.local>
2018-07-01 22:06:22 +0300

    icon

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 		  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet">
+	<link rel="shortcut icon" type="image/x-icon" href="template/img/favicon.ico"/>
 <!-- 	<link href="template/css/style.css" rel="stylesheet" type="text/css"> -->
 	<style>
 		<?php include (ROOT.'/template/css/style.css');?>
