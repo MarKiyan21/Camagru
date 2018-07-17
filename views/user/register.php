2018f5b6e4a627e5e51e21ed28dd7358b5f26d91
MarKiyan21 <MarianKyianytsia@gmail.com>
2018-07-17 19:05:42 +0300

    asd

--- views/user/register.php
+++ views/user/register.php
@@ -4,8 +4,8 @@
 	<div class="container">
 		<h1>Welcome to Camagru</h1>
 		<form id="register-from" action="#" method="post">
-			<input id="nickname" class="<?php echo $erLogin ?>" type="text" name="login" value="<?php echo $login ?>" placeholder="Username" required>
-			<input id="email" class="<?php echo $erEmail ?>" type="email" name="email" value="<?php echo $email ?>" placeholder="Email" required>
+			<input id="nickname" class="<?php echo $erLogin ?>" type="text" name="login" value="<?php echo $login ?>" placeholder="Username" autocomplete="off" required>
+			<input id="email" class="<?php echo $erEmail ?>" type="text" name="email" value="<?php echo $email ?>" placeholder="Email" autocomplete="off" required>
 			<input id="new-password" class="<?php echo $erPass ?>" type="password" name="newpass" placeholder="Password" required>
 			<input id="conf-password" class="<?php echo $erPass ?>" type="password" name="confpass" placeholder="Confirm password" required>
 			<button id="register-button" type="submit" name="submit">Register</button>
