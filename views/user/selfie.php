7a77bfa62798afaa6576079062d7cc78d70ca762
marian <marian@MacBook-Pro-Mac.local>
2018-08-07 22:48:43 +0300

    selfie 0.01

--- views/user/selfie.php
+++ views/user/selfie.php
@@ -1,9 +1,11 @@
 <?php include (ROOT.'/views/layouts/header.php');?>
 
-<div class="row">
+<div class="area">
 
 	<div class="photo-area col-md-6 col-sm-6 col-xs-10 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
-
+		<div class="buffer">
+			<video id="video" width="100%" height="100%"></video>
+		</div>
 	</div>
 	
 	<div class="stickers-area col-md-4 col-sm-4 col-xs-10 col-md-offset-0 col-sm-offset-0 col-xs-offset-1">
@@ -12,6 +14,6 @@
 	
 </div>
 
-<script src="/template/js/profile.js"></script>
+<script src="/template/js/selfie.js"></script>
 
 <?php include (ROOT.'/views/layouts/footer.php');?>
