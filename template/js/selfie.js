a802984e3ccf1f1b65029c0dcdd76979da48fe68
marian <marian@MacBook-Pro-Mac.local>
2018-08-12 23:24:30 +0300

    with stickers and previous showing in canvas

--- template/js/selfie.js
+++ template/js/selfie.js
@@ -1,11 +1,18 @@
+var globalWidth = 50;
+var globalX = 25;
+var globalY = 0;
+var stickerWidth = 0;
+var stickerHeight = 0;
+
 (function() {
 	var video = document.getElementById('video'),
 		canvas = document.getElementById('canvas'),
-		context = canvas.getContext('2d');
-		setAvatar = document.getElementById('set-as-avatar');
-		savePhoto = document.getElementById('save-photo');
-		capture = document.getElementById('capture');
-		bttnArea = document.getElementsByClassName('button-area')[0];
+		context = canvas.getContext('2d'),
+		setAvatar = document.getElementById('set-as-avatar'),
+		savePhoto = document.getElementById('save-photo'),
+		capture = document.getElementById('capture'),
+		bttnArea = document.getElementsByClassName('button-area')[0],
+		sticker = document.getElementById('sticker'),
 		vendorUrl = window.URL || window.webkitURL;
 		
 	navigator.getMedia = 	navigator.getUserMedia ||
@@ -30,6 +37,83 @@
 		savePhoto.style.display = "inline-grid";
 		canvas.style.display = "inline-grid";
 		context.drawImage(video, 0, 0, 640, 480);
+		console.log(stickerWidth);
+		console.log(stickerHeight);
+		context.drawImage(sticker, globalX * 2 - 25, globalY * 2, stickerWidth * 2, stickerHeight * 2);
 	})
 	
 })();
+
+function imposePhoto(element) {
+	var dataId = element.dataset.id,
+		canvas = document.getElementById('canvas'),
+		sticker = document.getElementById('sticker'),
+		context = sticker.getContext('2d'),
+		buffer = document.getElementById('sticker-buff');
+
+	sticker.dataset.flag = 1;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	globalX = 25;
+	globalY = 0;
+	globalWidth = 50;
+	stickerWidth = 50;
+	stickerHeight = 50;
+	sticker.style.top = "0";
+	sticker.style.left = "25px";
+	sticker.style.width = "50px";
+	context.drawImage(element, 0, 0, 50, 50);
+}
+
+var sticker = document.getElementById('sticker');
+if (sticker) {
+	window.addEventListener("keydown", function(e) {
+		if (sticker.dataset.flag == 1) {
+			if (e.keyCode == 187) {
+				globalWidth += 5;
+				if (globalWidth > 320) {
+					globalWidth = 320;
+				}
+				sticker.style.width = globalWidth + "px";
+				stickerWidth = sticker.offsetWidth;
+				stickerHeight = sticker.offsetHeight;
+			}
+			if (e.keyCode == 189) {
+				globalWidth -= 5;
+				if (globalWidth < 25) {
+					globalWidth = 25;
+				}
+				sticker.style.width = globalWidth + "px";
+				stickerWidth = sticker.offsetWidth;
+				stickerHeight = sticker.offsetHeight;
+			}
+			if (e.keyCode == 87) {
+				globalY -= 5;
+				if (globalY < 0) {
+					globalY = 0;
+				}
+				sticker.style.top = globalY + "px";
+			}
+			if (e.keyCode == 83) {
+				globalY += 5;
+				if (globalY > 170) {
+					globalY = 170;
+				}
+				sticker.style.top = globalY + "px";
+			}
+			if (e.keyCode == 65) {
+				globalX -= 5;
+				if (globalX < 25) {
+					globalX = 25;
+				}
+				sticker.style.left = globalX + "px";
+			}
+			if (e.keyCode == 68) {
+				globalX += 5;
+				if (globalX > 275) {
+					globalX = 275;
+				}
+				sticker.style.left = globalX + "px";
+			}
+		}
+	})
+}
--- views/user/selfie.php
+++ views/user/selfie.php
@@ -5,6 +5,9 @@
 	<div class="photo-area col-md-5 col-sm-5 col-xs-10 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
 		<div class="buffer">
 			<video id="video" width="640" height="480"></video>
+			<div id="sticker-buff">
+				<canvas id="sticker" data-flag="0" width="50" height="50"></canvas>
+			</div>
 			<canvas id="canvas" width="640" height="480"></canvas>
 			<div class="button-area">
 				<button id="capture" class="btn btn-info buff-capture-button">Take photo</button>
@@ -19,37 +22,37 @@
 			
 			<div class="column">
 					<div>
-						<img class="img-responsive" src="/assets/stickers/1.svg">
+						<img data-id="1" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/1.svg">
 					</div>
 					<div>
-						<img class="img-responsive" src="/assets/stickers/4.svg">
+						<img data-id="4" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/4.svg">
 					</div>
 					<div>
-						<img class="img-responsive" src="/assets/stickers/7.svg">
-					</div>
-			</div>
-			
-			<div class="column">
-					<div>
-						<img class="img-responsive" src="/assets/stickers/2.png">
-					</div>
-					<div>
-						<img class="img-responsive" src="/assets/stickers/5.svg">
-					</div>
-					<div>
-						<img class="img-responsive" src="/assets/stickers/8.svg">
+						<img data-id="7" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/7.svg">
 					</div>
 			</div>
 			
 			<div class="column">
 					<div>
-						<img class="img-responsive" src="/assets/stickers/3.svg">
+						<img data-id="2" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/2.png">
 					</div>
 					<div>
-						<img class="img-responsive" src="/assets/stickers/6.svg">
+						<img data-id="5" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/5.svg">
 					</div>
 					<div>
-						<img class="img-responsive" src="/assets/stickers/9.svg">
+						<img data-id="8" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/8.svg">
+					</div>
+			</div>
+			
+			<div class="column">
+					<div>
+						<img data-id="3" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/3.svg">
+					</div>
+					<div>
+						<img data-id="6" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/6.svg">
+					</div>
+					<div>
+						<img data-id="9" class="img-responsive" onclick="imposePhoto(this)" src="/assets/stickers/9.svg">
 					</div>
 			</div>
 			
