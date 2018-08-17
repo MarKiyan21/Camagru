8146d75e657ed28305d3d0e12aa068a983f955f4
MarKiyan21 <MarianKyianytsia@gmail.com>
2018-08-17 16:15:48 +0300

    asd

--- views/user/info.php
+++ views/user/info.php
@@ -141,85 +141,91 @@
 <div class="row">
 	
 	<div class="collage horizontal col-md-9 col-sm-8 col-xs-8 col-sm-offset-1 col-md-offset-1 col-xs-offset-1">
-	  
-		<div class="row">
-			
-			<div class="column">
-				<?php $i = 0; while ($i < count($user['photos'])): ?>
-					<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
-						<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
-						<div class="gallery-item-info">
-							<ul>
-								<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
-								<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
-							</ul>
-						</div>
-					</div>
-				<?php $i += 5; ?>
-				<?php endwhile; ?>
+		
+		<?php if (count($user['photos']) == 0): ?>
+			<div class="col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+				<h3>No photos at this moment</h3>
 			</div>
-			
-			<div class="column">
-				<?php $i = 1; while ($i < count($user['photos'])): ?>
-					<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
-						<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
-						<div class="gallery-item-info">
-							<ul>
-								<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
-								<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
-							</ul>
+		<?php else: ?>
+			<div class="row">
+				
+				<div class="column">
+					<?php $i = 0; while ($i < count($user['photos'])): ?>
+						<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
+							<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
+							<div class="gallery-item-info">
+								<ul>
+									<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
+									<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
+								</ul>
+							</div>
 						</div>
-					</div>
-				<?php $i += 5; ?>
-				<?php endwhile; ?>
-			</div>
-			
-			<div class="column">
-				<?php $i = 2; while ($i < count($user['photos'])): ?>
-					<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
-						<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
-						<div class="gallery-item-info">
-							<ul>
-								<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
-								<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
-							</ul>
+					<?php $i += 5; ?>
+					<?php endwhile; ?>
+				</div>
+				
+				<div class="column">
+					<?php $i = 1; while ($i < count($user['photos'])): ?>
+						<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
+							<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
+							<div class="gallery-item-info">
+								<ul>
+									<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
+									<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
+								</ul>
+							</div>
 						</div>
-					</div>
-				<?php $i += 5; ?>
-				<?php endwhile; ?>
-			</div>
-			
-			<div class="column">
-				<?php $i = 3; while ($i < count($user['photos'])): ?>
-					<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
-						<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
-						<div class="gallery-item-info">
-							<ul>
-								<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
-								<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
-							</ul>
+					<?php $i += 5; ?>
+					<?php endwhile; ?>
+				</div>
+				
+				<div class="column">
+					<?php $i = 2; while ($i < count($user['photos'])): ?>
+						<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
+							<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
+							<div class="gallery-item-info">
+								<ul>
+									<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
+									<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
+								</ul>
+							</div>
 						</div>
-					</div>
-				<?php $i += 5; ?>
-				<?php endwhile; ?>
-			</div>
-			
-			<div class="column">
-				<?php $i = 4; while ($i < count($user['photos'])): ?>
-					<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
-						<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
-						<div class="gallery-item-info">
-							<ul>
-								<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
-								<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
-							</ul>
+					<?php $i += 5; ?>
+					<?php endwhile; ?>
+				</div>
+				
+				<div class="column">
+					<?php $i = 3; while ($i < count($user['photos'])): ?>
+						<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
+							<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
+							<div class="gallery-item-info">
+								<ul>
+									<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
+									<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
+								</ul>
+							</div>
 						</div>
-					</div>
-				<?php $i += 5; ?>
-				<?php endwhile; ?>
+					<?php $i += 5; ?>
+					<?php endwhile; ?>
+				</div>
+				
+				<div class="column">
+					<?php $i = 4; while ($i < count($user['photos'])): ?>
+						<div class="gallery-item" onclick="showDetails(<?php echo $user['photos'][$i]['image_id']; ?>)">
+							<img class="img-responsive" src="<?php echo $user['photos'][$i]['path']; ?>">
+							<div class="gallery-item-info">
+								<ul>
+									<li class="gallery-item-likes"><i class="im im-icon-Heart-2"></i> <?php echo $user['photos'][$i]['likes']; ?></li>
+									<li class="gallery-item-comments"><i class="im im-icon-Speach-Bubble"></i> <?php echo $user['photos'][$i]['comments']; ?></li>
+								</ul>
+							</div>
+						</div>
+					<?php $i += 5; ?>
+					<?php endwhile; ?>
+				</div>
+				
 			</div>
-			
-		</div>
+		<?php endif; ?>
 	  
 	</div>
 	
